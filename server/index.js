82b06ae846b0302e6dfdb43c076649d6926eefcf
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-09-16 04:59:25 +0800

    Fix typo

--- server/index.js
+++ server/index.js
@@ -6,15 +6,17 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import ProteinInteractionsRoutes from "./routes/proteinInteractions.js";
+import ProteinInteraction from "./models/ProteinInteraction.js";
+import { jsonData } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
@@ -23,13 +25,13 @@ app.use("/api/protein-interactions", ProteinInteractionsRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
-
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(async () => {
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    //ProteinInteraction.insertMany(jsonData);
   })
   .catch((error) => console.log(`${error} did not connect`));
