281653c99c7037769782a4e1d479982f90442fa8
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-09-16 04:59:47 +0800

    Fix styling error

--- server/models/ProteinInteraction.js
+++ server/models/ProteinInteraction.js
@@ -2,23 +2,25 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const ProteinInteractionSchema = new Schema(
-  {
-    pdb_id: String,
-    mutations: String,
-    protein1: String,
-    protein2: String,
-    experiment: String,
-    temperature: Number,
-    ph: Number,
-    delta_g: Number,
-    delta_delta_g: Number,
-    authors: String,
-    journal: String,
-    pubmed_id: String
-  }
+const ProteinInteractionSchema = new Schema({
+  pdb_id: String,
+  mutations: String,
+  protein1: String,
+  protein2: String,
+  experiment: String,
+  temperature: Number,
+  ph: Number,
+  delta_g: Number,
+  delta_delta_g: Number,
+  authors: String,
+  journal: String,
+  pubmed_id: String,
+});
+
+const ProteinInteraction = mongoose.model(
+  "ProteinInteraction",
+  ProteinInteractionSchema,
+  "protein_interactions"
 );
 
-const ProteinInteraction = mongoose.model("ProteinInteraction", ProteinInteractionSchema, "protein_interactions")
-
-export default ProteinInteraction
+export default ProteinInteraction;
