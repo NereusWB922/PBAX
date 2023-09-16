207ae9f0341053983660db241dd5a2868df44507
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-09-16 17:23:25 +0800

    Add type for query response and param

---
+++ client/src/state/types.ts
@@ -0,0 +1,33 @@
+export interface SearchProtienInteractionsResponse {
+  proteinInteractions: ProtienInteraction[];
+  total: number;
+}
+
+export interface ProtienInteraction {
+  pdb_id: string;
+  mutations: string;
+  protein1: string;
+  protein2: string;
+  experiment: string;
+  temperature: number;
+  ph: number;
+  delta_g: number;
+  delta_delta_g: number;
+  authors: string;
+  journal: string;
+  pubmed_id: string;
+  __v: number;
+  _id: number;
+}
+
+export interface SearchProtienInteractionsParam {
+  page: number;
+  pageSize: number;
+  sort: string;
+  search: string;
+}
+
+export interface sortModel {
+  field: string;
+  sort: "asc" | "desc";
+}
