a5db7afd3278b02d774964804c952c79fff21ed0
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-09-25 22:36:42 +0800

    Create advancedSearchModel

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -25,3 +25,34 @@ export interface SearchProtienInteractionsParam {
   sort: string;
   search: string;
 }
+
+export interface AdvancedSearchModel {
+  protein1: string | null;
+  protein2: string | null;
+  pbd_id: string | null;
+  type: "wild" | "mutant" | null;
+  mutate_from: string | null;
+  mutate_to: string | null;
+  experiment: string | null;
+  max_temp: number | null;
+  min_temp: number | null;
+  max_ph: number | null;
+  min_ph: number | null;
+  max_delta_g: number | null;
+  min_delta_g: number | null;
+  max_delta_delta_g: number | null;
+  min_delta_delta_g: number | null;
+  authors: string | null;
+  journal: string | null;
+  pubmed_id: string | null;
+}
+
+export interface handleFieldChangeProp {
+  field: string;
+  value: string | number | null;
+}
+
+export interface Option {
+  label: string;
+  value: string;
+}
