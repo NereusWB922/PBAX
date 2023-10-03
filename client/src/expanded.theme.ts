93ad112273a0209d4973ed57a9e5f717a3629298
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-10-03 20:36:49 +0800

    Fix linting error

--- client/src/expanded.theme.ts
+++ client/src/expanded.theme.ts
@@ -1,12 +1,12 @@
-// eslint-disable-next-line
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
-    interface PaletteColor {
-        [key: number]: string;
-    }
+  interface PaletteColor {
+    [key: number]: string;
+  }
 
-    interface Palette {
-        tertiary: PaletteColor;
-    }
+  interface Palette {
+    tertiary: PaletteColor;
+  }
 }
