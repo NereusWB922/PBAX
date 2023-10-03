faa6e68fe26d7bbca141132fddc7ac5604007b70
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2023-10-03 16:44:37 +0800

    Replace backdrop with dialog component

--- client/src/components/AdvancedSearchForm/index.tsx
+++ client/src/components/AdvancedSearchForm/index.tsx
@@ -1,4 +1,12 @@
-import { Button, Card, Grid, Typography, useTheme } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  Grid,
+  IconButton,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import ProteinInfoSection from "./ProteinInfoSection";
 import { initialAdvancedSearchModel } from "@/scenes/search/initialValues";
 import { AdvancedSearchModel, handleFieldChangeProp } from "@/types/types";
@@ -7,17 +15,20 @@ import ExperimentalConditionsSection from "./ExperimentalConditionsSection";
 import ThermodynamicParametersSection from "./ThermodynamicParametersSection";
 import LiteratureInfoSection from "./LiteratureInfoSection";
 import FlexBetween from "@/common/FlexBetween";
+import CloseIcon from "@mui/icons-material/Close";
 
 type Props = {
   closeSearchForm: () => void;
   setAdvancedSearch: Dispatch<AdvancedSearchModel>;
   advancedSearchModel: AdvancedSearchModel;
+  showSearchForm: boolean;
 };
 
 const AdvancedSearchForm = ({
   closeSearchForm,
   setAdvancedSearch,
   advancedSearchModel,
+  showSearchForm,
 }: Props) => {
   const theme = useTheme();
 
@@ -50,75 +61,94 @@ const AdvancedSearchForm = ({
 
   return (
     <form style={{ width: "34%" }} onSubmit={(event) => event.preventDefault()}>
-      <Card
-        sx={{
-          width: "100%",
-          padding: "2rem 2.5rem",
-          backgroundColor: theme.palette.grey[100],
-          maxHeight: "70vh",
-          overflow: "auto",
-          "&::-webkit-scrollbar": {
-            width: "0.5em",
-            overflow: "auto",
-          },
-          "&::-webkit-scrollbar-track": {
-            backgroundColor: theme.palette.grey[800],
-          },
-          "&::-webkit-scrollbar-thumb": {
-            background: theme.palette.primary[400],
-            borderRadius: "20px",
-            backgroundClip: "content-box",
-          },
-          "&::-webkit-scrollbar-thumb:hover": {
-            background: theme.palette.primary[600],
+      <Dialog
+        onClose={onClose}
+        open={showSearchForm}
+        maxWidth="xs"
+        fullWidth
+        PaperProps={{
+          sx: {
+            height: "70vh",
           },
         }}
       >
-        <Typography
-          variant="h2"
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
           sx={{
-            color: theme.palette.grey[900],
-            fontWeight: "600",
-            mb: "1rem",
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
           }}
         >
-          Advanced Search
-        </Typography>
-        <ProteinInfoSection
-          tempAdvancedSearchModel={tempAdvancedSearchModel}
-          handleFieldChange={handleFieldChange}
-        />
-        <ExperimentalConditionsSection
-          tempAdvancedSearchModel={tempAdvancedSearchModel}
-          handleFieldChange={handleFieldChange}
-        />
-        <ThermodynamicParametersSection
-          tempAdvancedSearchModel={tempAdvancedSearchModel}
-          handleFieldChange={handleFieldChange}
-        />
-        <LiteratureInfoSection
-          tempAdvancedSearchModel={tempAdvancedSearchModel}
-          handleFieldChange={handleFieldChange}
-        />
-        <Grid container justifyContent="center" sx={{ mt: "2rem" }}>
-          <FlexBetween gap="1rem">
-            <Button variant="outlined" onClick={onClose} size="large">
-              Close
-            </Button>
-            <Button
-              variant="outlined"
-              onClick={onClear}
-              size="large"
-              color="error"
-            >
-              Clear
-            </Button>
-            <Button variant="contained" onClick={onSubmit} size="large">
-              Submit
-            </Button>
-          </FlexBetween>
-        </Grid>
-      </Card>
+          <CloseIcon />
+        </IconButton>
+        <DialogContent
+          sx={{
+            "&::-webkit-scrollbar": {
+              width: "0.5em",
+              overflow: "auto",
+            },
+            "&::-webkit-scrollbar-track": {
+              backgroundColor: theme.palette.grey[800],
+            },
+            "&::-webkit-scrollbar-thumb": {
+              background: theme.palette.primary[400],
+              borderRadius: "20px",
+              backgroundClip: "content-box",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              background: theme.palette.primary[600],
+            },
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              color: theme.palette.grey[900],
+              fontWeight: "600",
+              mb: "1rem",
+            }}
+          >
+            Advanced Search
+          </Typography>
+          <ProteinInfoSection
+            tempAdvancedSearchModel={tempAdvancedSearchModel}
+            handleFieldChange={handleFieldChange}
+          />
+          <ExperimentalConditionsSection
+            tempAdvancedSearchModel={tempAdvancedSearchModel}
+            handleFieldChange={handleFieldChange}
+          />
+          <ThermodynamicParametersSection
+            tempAdvancedSearchModel={tempAdvancedSearchModel}
+            handleFieldChange={handleFieldChange}
+          />
+          <LiteratureInfoSection
+            tempAdvancedSearchModel={tempAdvancedSearchModel}
+            handleFieldChange={handleFieldChange}
+          />
+          <Grid container justifyContent="center" sx={{ mt: "2rem" }}>
+            <FlexBetween gap="1rem">
+              <Button variant="outlined" onClick={onClose} size="large">
+                Close
+              </Button>
+              <Button
+                variant="outlined"
+                onClick={onClear}
+                size="large"
+                color="error"
+              >
+                Clear
+              </Button>
+              <Button variant="contained" onClick={onSubmit} size="large">
+                Submit
+              </Button>
+            </FlexBetween>
+          </Grid>
+        </DialogContent>
+      </Dialog>
     </form>
   );
 };
